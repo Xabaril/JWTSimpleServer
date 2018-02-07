50196e3b46c87f876ff78bf8fc4c256eba521a98
Carlos Landeras <carlos.landeras@gmail.com>
2018-02-08 00:18:17 +0100

    Create InMemoryRefresthTokenStore project

---
+++ src/JWTSimpleServer.InMemoryRefreshTokenStore/InMemoryRefreshTokenStore.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using JWTSimpleServer.Abstractions;
+
+
+namespace JWTSimpleServer.InMemoryRefreshTokenStore
+{
+    public class InMemoryRefreshTokenStore : IRefreshTokenStore
+    {
+        public string GenerateRefreshToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Token> GetTokenAsync(string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task InvalidateRefreshTokenAsync(string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StoreTokenAsync(Token token)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

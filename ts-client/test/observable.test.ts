b44bf305c2178241e94d647b59e79a83de9bdbd5
Carlos Landeras <carlos.landeras@gmail.com>
2018-02-08 15:28:36 +0100

    Observable and Observer test

--- ts-client/test/observable.test.ts
+++ ts-client/test/observable.test.ts
@@ -1,12 +1,37 @@
 import { Observable } from "../src/observable";
 
-describe("Observable should not have observers when created", () => {
-        let observable = new Observable<string>();        
-        expect(observable.hasObservers()).toBe(false);
-});
+describe("Observable should", () => {
+        it("Not have observers when first created", () => {
+                let observable = new Observable<string>();
+                expect(observable.hasObservers()).toBe(false);
+        });
 
-describe("Observable should have observers when subscribed", () => {
-        let observable = new Observable<string>();
-        observable.subscribe( (data: string) => {});
-        expect(observable.hasObservers()).toBe(true);
+        it("should have observers when subscribed", () => {
+                let observable = new Observable<string>();
+                observable.subscribe((data: string) => { });
+                expect(observable.hasObservers()).toBe(true);
+        });        
+
+        it("should notify observers when published", () => {
+                let observable = new Observable<Date>();
+                let mockObserver = jest.fn();
+                
+                let date = new Date();
+                observable.subscribe(mockObserver);
+                observable.notify(date);
+
+                expect(mockObserver).toHaveBeenCalledWith(date);
+        });
+
+        it('should remove an observer', () => {
+                let observable = new Observable<string>();
+                let observer = observable.subscribe((str: string) => {});
+
+                expect(observable.hasObservers()).toBe(true);
+
+                let removed = observable.remove(observer);
+                expect(removed).toBe(true);
+                expect(observable.hasObservers()).toBe(false);
+        })
+
 });

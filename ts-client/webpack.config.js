bead113f222f17f21a87b7e03cabdf9c44fcba84
Carlos Landeras <carlos.landeras@gmail.com>
2018-02-09 10:13:57 +0100

    Export browser library target to namespace module

--- ts-client/webpack.config.js
+++ ts-client/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: "simple-server-client.js",
+        library : 'JwtSimpleServer',
         libraryTarget: 'window'
     },
     resolve: {

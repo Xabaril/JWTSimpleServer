9d248c2659d4f559826b2913e461fb67593dea7b
Carlos Landeras <carlos.landeras@gmail.com>
2018-02-13 10:57:22 +0100

    Add distributed redis token store implementation

---
+++ src/JWTSimpleServer.RedisDistributedRefreshTokenStore/RedisDistributedRefreshTokenStore.cs
@@ -0,0 +1,44 @@
+﻿using JWTSimpleServer;
+using JWTSimpleServer.Abstractions;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class RedisDistributedRefreshTokenStore : IRefreshTokenStore
+    {
+        private readonly IDistributedCache _distributedCache;
+        private const string TOKEN_CACHE_KEY = "JWT_SERVER_TOKEN_";
+
+        public RedisDistributedRefreshTokenStore(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<Token> GetTokenAsync(string refreshToken)
+        {
+            var token = await _distributedCache.GetStringAsync($"{TOKEN_CACHE_KEY}{refreshToken}");
+            return string.IsNullOrEmpty(token) ? null : JsonConvert.DeserializeObject<Token>(token);
+        }
+
+        public Task InvalidateRefreshTokenAsync(string refreshToken)
+        {
+            return _distributedCache.RemoveAsync($"{TOKEN_CACHE_KEY}{refreshToken}");
+        }
+
+        public Task StoreTokenAsync(Token token)
+        {
+            var serializedToken = JsonConvert.SerializeObject(token);
+            return _distributedCache.SetStringAsync($"{TOKEN_CACHE_KEY}{token.RefreshToken}", serializedToken);
+        }
+        public string GenerateRefreshToken()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+    }
+}
---
+++ src/JWTSimpleServer.RedisDistributedRefreshTokenStore/RedisRefreshTokenStoreServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JWTSimpleServer.Abstractions;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class RedisRefreshTokenStoreServiceCollectionExtensions
+    {
+        const string JwtSimpleServerRedisInstanceName = "JwtSimpleServerInstance";
+        public static IServiceCollection AddDistributedRedisRefreshStokenStore(
+            this IServiceCollection services,
+            Action<RedisCacheOptions> cacheSetup
+        )
+        {           
+            services.AddSingleton<IRefreshTokenStore, RedisDistributedRefreshTokenStore>();            
+            services.AddDistributedRedisCache(setup => cacheSetup(setup));            
+            return services;
+        }        
+    }
+}
